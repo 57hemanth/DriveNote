98a0a2ec1113539df2f50433c6265e507a28b295
57hemanth <hemanthchowdary1234@gmail.com>
2025-03-29 14:09:07 +0530

    Made few design changes and added vercel.json file

--- client/src/components/homepage/Hero.tsx
+++ client/src/components/homepage/Hero.tsx
@@ -5,10 +5,10 @@ export default function Hero(){
         <div className="flex flex-col justify-center items-center mt-20 gap-6">
             <h1 className="text-4xl max-w-[560px] text-center leading-12 font-medium">Effortless Writing. Instant Saving. Anywhere, Anytime</h1>
             <p className="max-w-[600px] text-center">Create and edit letters with ease, seamlessly save them to Google Drive, and access them anytime from any device.</p>
-            <div className="flex flex-row gap-4 border px-4 py-2 rounded-full">
-                <input className="w-[240px] outline-none" placeholder="Enter your email address" />
+            <div className="flex flex-row gap-1 md:gap-4 border px-4 py-2 rounded-full">
+                <input className="md:w-[240px] outline-none text-sm" placeholder="Enter your email address" />
                 <Link to={"/login"} className="bg-primary px-4 py-1 rounded-full text-white">
-                    <p>Get Started</p>
+                    <p className="text-sm overflow-ellipsis">Get Started</p>
                 </Link>
             </div>            
         </div>
